f6f70155b6029dd1041e66d78934f2970efe2539
Unesh Shahi <eunessshahithakuri@gmail.com>
2025-05-12 02:39:15 +0545

    chore: fixed the padding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export function Home() {
   return (
     <>
       <Header />
-      <main className="pt-32 px-4">
+      <main className="pt-24 px-4">
       <div className="max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl mx-auto text-center">
            <CardSlider />
        </div>
